240e71891994e3d8a42195c5b05302d23aaacd1c
shihab20105 <shihabur10@gmail.com>
2025-09-01 00:14:06 +0100

    Explored When to use getElementById and querySelector

---
+++ 30_5-1 When To Use GetElementById And QuerySelector/recap.js
@@ -0,0 +1,29 @@
+/*                      30_5-1 When to use getElementById and querySelector */
+
+/* 
+1. How to use JavaScript with HTML and CSS
+
+2. Get element or elements from DOM
+    2-1: getElementById 
+    2-2: getElementsByClassName
+    2-3: querySelector
+    2-4: querySelectorAll
+
+3. Dynamically add element to the DOM: document.createElement
+    3-1: Set value to the innerHTML
+
+4. Get and set value from/to a DOM element 
+    4-1: Use innerText (h1, p, h5, div, span, strong)
+    4-2: Use value (input, textArea)
+
+5. addEventListener (click handler/event handler)
+    5-1: click, focus, blur, onchange, keyup
+    5-2: addEventListener onclick="myHandler()"
+
+6. Use function to reduce duplicate code
+
+7. Event bubble and event delegate
+*/
+
+document.querySelectorAll('.users-container .disable h3')
+document.querySelectorAll('.my-users li')
